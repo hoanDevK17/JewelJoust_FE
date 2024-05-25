89f3f7fb3c960ac2d1733d66d91db089c1969480
hoanDevK17 <hoannse170243@fpt.edu.vn>
2024-05-25 11:27:42 +0700

    fix css src img

--- src/component/authen-template/index.jsx
+++ src/component/authen-template/index.jsx
@@ -1,16 +1,16 @@
-import './index.scss'
+import "./index.scss";
 
-export default function AuthenTemplate({children}) {
-  return <div className="authen-template">
-    <div class="authen-template__content">
-      <div className="authen-template__content__form">
-          <div className="wrapper">
-              {children}
-          </div>
-      </div>
-      <div className="authen-template__content__background">
+export default function AuthenTemplate({ children }) {
+  return (
+    <div className="authen-template">
+      <div className="authen-template__content">
+        <div className="authen-template__content__form">
+          <div className="wrapper">{children}</div>
+        </div>
+        <div className="authen-template__content__background">
           <img src="/image.svg" alt="" />
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
--- src/page/registration/index.jsx
+++ src/page/registration/index.jsx
@@ -1,61 +1,95 @@
 import AuthenTemplate from "../../component/authen-template";
-import './index.scss'
-import logo from './src/Frame.svg';
-import google from './src/google.svg';
-import eye from './src/eye.svg';
+import ButtonPrimary from "../../component/button-primary/ButtonPrimary";
+import "./index.scss";
+import { useNavigate } from "react-router-dom";
 export default function Registration() {
-  return <AuthenTemplate>
-    <div class="big">
-      <div class ="logo-head">     
-        <img class="logo-icon" src={logo} alt=""/>      
-      </div>
-      <div class ="wellcome">
-        <p class="welcome-name">WELCONE JEWELJOUST!</p>
-        <p class="wellcome-mess">Enter your details to register your account !</p>
-      </div>
-      <button class="buttion-regis-google">
-        <img class="google-icon" src={google} alt="" />
-        <p class ="regis-google-text">Continue With Google</p>
-      </button>
-      <div class ="boxs-conten">
-        <div class="input-details">
-          <p class="conten-name">Username</p>
-          <input class="conten-mess" type="text" placeholder=" Enter your Username" />
+  const navigate = useNavigate();
+  return (
+    <AuthenTemplate>
+      <div className="big">
+        <div className="wellcome">
+          <p className="welcome-name">WELCONE JEWELJOUST!</p>
+          <p className="wellcome-mess">
+            Enter your details to register your account !
+          </p>
         </div>
-        <div class="input-pass">
-          <p class="conten-name">Password</p>
-          <div class="icon-mess">
-            <input class="conten-mess" type="password" placeholder=" Enter your Password"/>
-            <img class="icon-eye"src={eye} alt="" />
+        <button className="buttion-regis-google">
+          <img className="google-icon" src="public/IconGoogle.svg" alt="" />
+          <p className="regis-google-text">Continue With Google</p>
+        </button>
+        <div className="boxs-conten">
+          <div className="input-details">
+            <p className="conten-name">Username</p>
+            <input
+              className="conten-mess"
+              type="text"
+              placeholder=" Enter your Username"
+            />
+          </div>
+          <div className="input-details">
+            <p className="conten-name">Password</p>
+
+            <input
+              className="conten-mess"
+              type="password"
+              placeholder=" Enter your Password"
+            />
+          </div>
+          <div className="input-details">
+            <p className="conten-name">Confirm password</p>
+            <input
+              className="conten-mess"
+              type="password"
+              placeholder=" Enter your Password"
+            />
+          </div>
+          <div className="input-details">
+            <p className="conten-name">Full Name</p>
+            <input
+              className="conten-mess"
+              type="text"
+              placeholder=" Enter your Full Name"
+            />
+          </div>
+          <div className="input-details">
+            <p className="conten-name">Email address</p>
+            <input
+              className="conten-mess"
+              type="text"
+              placeholder=" Enter your Email address"
+            />
+          </div>
+          <div className="input-details">
+            <p className="conten-name">PhoneNumber</p>
+            <input
+              className="conten-mess"
+              type="text"
+              placeholder="Enter your PhoneNumber"
+            />
           </div>
         </div>
-        <div class="input-pass">
-          <p class="conten-name">Confirm password</p>
-          <div class="icon-mess">
-            <input class="conten-mess" type="password" placeholder=" Enter your Password"/>
-            <img class="icon-eye"src={eye} alt="" />
+        <div className="button-regis">
+          {/* <button className="button-star-regis">Register</button>
+           */}
+          <ButtonPrimary
+            title="ABCD"
+            Onclick={() => {
+              navigate("/login");
+            }}
+          />
+          <div className="login">
+            <p className="login-text">Already have an account?</p>
+            <span
+              className="login-link"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Log in
+            </span>
           </div>
         </div>
-        <div class="input-details">
-          <p class="conten-name">Full Name</p>
-          <input class="conten-mess" type="text" placeholder=" Enter your Full Name" />
-        </div>
-        <div class="input-details">
-          <p class="conten-name">Email address</p>
-          <input class="conten-mess" type="text" placeholder=" Enter your Email address" />
-        </div>
-        <div class="input-details">
-          <p class="conten-name">PhoneNumber</p>
-          <input class="conten-mess" type="text" placeholder="Enter your PhoneNumber" />
-        </div>
       </div>
-      <div class ="button-regis">
-        <button class="button-star-regis">register</button>
-        <div class="login">
-          <p class="login-text">Already have an account?</p>
-          <a class="login-link" href="http://localhost:5173/login">Log in</a>
-        </div>
-      </div>
-    </div>
-  </AuthenTemplate>
+    </AuthenTemplate>
+  );
 }
