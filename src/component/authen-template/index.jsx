be5c9d8dc525dfb51f3f3181c32bbabc8b89f017
hoanDevK17 <hoannse170243@fpt.edu.vn>
2024-05-30 23:18:35 +0700

    fix template img Onclick to homepage

--- src/component/authen-template/index.jsx
+++ src/component/authen-template/index.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "./index.scss";
 
 export default function AuthenTemplate({ children }) {
+  const navigate = useNavigate();
   return (
     <div className="authen-template">
       <div className="authen-template__content">
         <div className="authen-template__content__form">
           <div className="wrapper">
-            <img src="public/Logo.svg" />
+            <img
+              src="public/Logo.svg"
+              onClick={() => {
+                navigate("/homepage");
+                console.log("oke");
+              }}
+            />
             {children}
           </div>
         </div>
