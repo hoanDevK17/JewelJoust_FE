ff0025af72a21926379f6fac9d7bd8fcbbb8869f
Your Name <your_email@example.com>
2024-05-31 17:09:22 +0700

    feat/homepage : fix lần 2

--- src/component/home-default/home.jsx
+++ src/component/home-default/home.jsx
@@ -1,4 +1,12 @@
-import { EnvironmentOutlined, FacebookOutlined, InstagramOutlined, MailOutlined, PhoneOutlined, TwitterOutlined, YoutubeOutlined } from "@ant-design/icons";
+import {
+  EnvironmentOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  TwitterOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
 import "./home.scss";
 import { useNavigate } from "react-router-dom";
 
@@ -74,107 +82,7 @@ export default function HomePage({ children }) {
         </div>
       </div>
       <div className="home-page-body">{children}</div>
-      <div className="footer">
-        <div className="footer-content">
-          {" "}
-          <h4>Serenity </h4>
-          <p>
-          Uncover Radiant Beauty - Precious Gems at Our Auction House</p>
-          <div className="footer-logo">
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            <FacebookOutlined />
-          </span>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-           <InstagramOutlined />
-          </span>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-                       <TwitterOutlined   />
-
-          </span>
-          <span
-            className="span-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            <YoutubeOutlined />
-          </span>
-          </div>
-        </div>
-        <div className="footer-content">
-          <h4>Quick Links</h4>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            Home
-          </span>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            Jewelry Auction
-          </span>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            Products
-          </span>
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            News
-          </span>{" "}
-          <span
-            className="button-link"
-            onClick={() => {
-              console.log("111");
-              navigate("/homepage");
-            }}
-          >
-            About Us
-          </span>
-        </div>
-        <div className="footer-content">
-          <h4> Contact Us</h4>
-          <p> <PhoneOutlined /> 424-947-9877</p>
-          <p><MailOutlined /> Luminary.@gmail.com</p>
-          <p><EnvironmentOutlined /> 9256 Abigail Forges, Sao Tome and Principe</p>
-        </div>
-      </div>
+      
     </div>
   );
 }
--- src/page/homepage/homePage.jsx
+++ src/page/homepage/homePage.jsx
@@ -1,14 +1,20 @@
 // import HomePage from "../../component/home-default/home.jsx";
 
-import  HomePage  from "../../component/home-default/home.jsx";
+import Footer from "../../component/footer/footer.jsx";
+import HomePage from "../../component/home-default/home.jsx";
 
 export default function Home() {
   return (
-    <HomePage>
-      {/* <h1>222</h1> */}
+    <>
+      <HomePage>
+   {/* Sang code chỗ này nha */}
 
 
-      
+
+
     </HomePage>
+      <Footer />
+    </>
+    
   );
 }
