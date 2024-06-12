97de7123069e0db68b022b7fcbecec66feaeab1f
hoanDevK17 <hoannse170243@fpt.edu.vn>
2024-06-12 11:06:36 +0700

    fix avt homepage

--- src/component/authen-template/index.jsx
+++ src/component/authen-template/index.jsx
@@ -2,29 +2,25 @@ import { useNavigate } from "react-router-dom";
 import "./index.scss";
 
 export default function AuthenTemplate({ children }) {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="authen-template">
       <div className="authen-template__content">
         <div className="authen-template__content__form">
           <div className="wrapper">
             <img
-              src="public/Logo.svg"
-              onClick={()=>{
-                navigate('/')
+              src="./Logo.svg"
+              onClick={() => {
+                console.log("ok");
+                navigate("/homepage");
               }}
             />
             {children}
           </div>
         </div>
         <div className="authen-template__content__background">
-        <span
-            className="button-link"
-            onClick={() => {
-              navigate("/homepage");
-            }}
-          >
-           <img src="./Logo.svg" alt="" />
+          <span className="button-link">
+            <img src="./RingBackGround.svg" alt="" />
           </span>
         </div>
       </div>
