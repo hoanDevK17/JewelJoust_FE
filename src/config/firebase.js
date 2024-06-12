a3731b8900a8d763f0ad1b1362a38ffcd7a0a28d
Sang <hongocsang1306@gmail.com>
2024-06-12 17:15:54 +0700

    feat/fix-firebaseconfig

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,17 +6,19 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 // just change config below to start
 const firebaseConfig = {
-    apiKey: "AIzaSyDPnlLBDzBumaCoeFEn9u7WWAgezUUnus4",
-    authDomain: "jeweljoust.firebaseapp.com",
-    projectId: "jeweljoust",
-    storageBucket: "jeweljoust.appspot.com",
-    messagingSenderId: "437114918759",
-    appId: "1:437114918759:web:fd8f8fb4e9399e28d0c1c2",
-    measurementId: "G-ZRY8BGXMJH"
-  };
+  apiKey: "AIzaSyBgddcANG0Ak2i7NC578-l9h0vP3-4jm9Y",
+  authDomain: "jeweljoust-c9e11.firebaseapp.com",
+  projectId: "jeweljoust-c9e11",
+  storageBucket: "jeweljoust-c9e11.appspot.com",
+  messagingSenderId: "469105945405",
+  appId: "1:469105945405:web:51a01e498079e015e23354",
+  measurementId: "G-40PKPFT3KM"
+};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
 export const provider = new GoogleAuthProvider();
+
+
