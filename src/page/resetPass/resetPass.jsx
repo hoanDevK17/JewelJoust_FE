2e64d6407a8b51e65d4584974ad9bb46e8cb06fa
Sang <hongocsang1306@gmail.com>
2024-06-15 02:03:53 +0700

    feat/reset-pass

--- src/page/resetPass/resetPass.jsx
+++ src/page/resetPass/resetPass.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+
 import AuthenTemplate from "../../component/authen-template";
-import { Form, useSearchParams } from "react-router-dom";
-import { Button, Input } from "antd";
+import { useSearchParams } from "react-router-dom";
+import { Button, Form, Input } from "antd";
 import { APIResetPass } from "../../api/api";
 
 
@@ -9,17 +9,84 @@ import { APIResetPass } from "../../api/api";
 export default function ResetPass() {
     const [searchParams] = useSearchParams();
     const token = searchParams.get("token");
-    const handleSubmit = ()=>{APIResetPass("123456",token).then((rs)=>{console.log(rs)})}
-
+    const handleSubmit = () => { APIResetPass("123456", token).then((rs) => { console.log(rs) }) }
     // console.log(token)
-    return (<AuthenTemplate>
-   <button onClick={handleSubmit}>Submitg</button>
+
    
-     
+    return (<AuthenTemplate>
+        {/* <button onClick={handleSubmit}>Submitg</button> */}
+        <div >
+            <h1>Please enter your new password!</h1>
+            <div className="reset-form" >
+                <Form className="form-reset"
+                    labelCol={{
+                        span: 24,
+                    }}
+                    onFinish={(values) => {
+                        console.log(values)
+                    }}>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="confirm"
+                        label="Confirm Password"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please confirm your password!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(new Error('The new password that you entered do not match!'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        style={{
+                            backgroundColor: "#ffbe98",
+                            border: "solid 4px #ffbe98",
+                            color: "#ffffff",
+                            borderRadius: "20px",
+                            fontFamily: "Poppins",
+                            fontSize: "16px",
+                            fontStyle: "normal",
+                            fontWeight: "600",
+                            lineHeight: "normal",
+                            width: "100%",
+                            textAlign: "center",
+                        }}
+                    >
+                        Login
+                    </Button>
+                </Form>
+            </div>
+        </div>
     </AuthenTemplate>)
 
- }
-    
-   
+}
+
+
 
 // pretieer
