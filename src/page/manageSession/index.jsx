028e3276945852f2e29ddad1c5ca3c43410c2e09
22H31 <quangdmse170316@fpt.edu.vn>
2024-06-29 10:19:56 +0700

    fix master

--- src/page/manageSession/index.jsx
+++ src/page/manageSession/index.jsx
@@ -426,7 +426,7 @@ export default function ManageSession() {
                   showSearch
                   placeholder="Select a person"
                   filterOption={(input, staff) => {
-                    return (staff?.label ?? "")q
+                    return (staff?.label ?? "")
                       .toLowerCase()
                       .includes(input.toLowerCase());
                   }}
